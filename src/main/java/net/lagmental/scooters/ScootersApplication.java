b54a866d93685fb647bfa6135813d4dad66f88a2
Vicfred <vicfredboy@gmail.com>
2019-09-20 22:38:21 -0500

    initial commit, basic spring boot

---
+++ src/main/java/net/lagmental/scooters/ScootersApplication.java
@@ -0,0 +1,13 @@
+package net.lagmental.scooters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScootersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScootersApplication.class, args);
+    }
+
+}
