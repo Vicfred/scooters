e385bea58f2e5e551ab68ce61ddddae2762696d8
Vicfred <vicfredboy@gmail.com>
2019-09-22 14:37:07 -0500

    added DB stuff and /setup endpoint

--- src/main/java/net/lagmental/scooters/ScooterController.java
+++ src/main/java/net/lagmental/scooters/ScooterController.java
@@ -1,14 +1,42 @@
 package net.lagmental.scooters;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ScooterController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @PostMapping(path = "/create", consumes = "application/json", produces = "application/json")
     public Scooter create(@RequestBody Scooter scooter) {
-        return new Scooter(scooter.getScooterid(), scooter.getEmail(), "CREATED");
+        String scooterid = scooter.getScooterid();
+        String email = scooter.getEmail();
+        jdbcTemplate.update("INSERT INTO scooters VALUES (?, ?, ?)", scooterid, email, "CREATED");
+        return new Scooter(scooterid, email, "CREATED");
+    }
+
+    @PostMapping(path = "/setup", consumes = "application/json", produces = "application/json")
+    public Scooter setup(@RequestBody Scooter scooter) {
+        String scooterid = scooter.getScooterid();
+        String email = scooter.getEmail();
+        String status = jdbcTemplate.queryForObject("SELECT status FROM scooters WHERE scooterid = ?",
+                new Object[]{scooterid}, String.class);
+
+        // TODO think how to handle the error
+        if("CREATED" != status) {
+            log.info("invalid status");
+            return null;
+        }
+
+        jdbcTemplate.update("UPDATE scooters SET status=? where scooterid = ?", "MAINTENANCE", scooterid);
+
+        return new Scooter(scooterid, email, "MAINTENANCE");
     }
 }
--- src/main/java/net/lagmental/scooters/ScootersApplication.java
+++ src/main/java/net/lagmental/scooters/ScootersApplication.java
@@ -1,13 +1,33 @@
 package net.lagmental.scooters;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class ScootersApplication {
+@Slf4j
+public class ScootersApplication implements CommandLineRunner {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(ScootersApplication.class, args);
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE scooters IF EXISTS");
+        jdbcTemplate.execute("DROP TABLE transactions IF EXISTS");
+        // FIXME scooterid should be unique
+        jdbcTemplate.execute("CREATE TABLE scooters(" +
+                "scooterid VARCHAR(255) NOT NULL PRIMARY KEY, email VARCHAR(255), status VARCHAR(255))");
+        jdbcTemplate.execute("CREATE TABLE transactions(" +
+                "event VARCHAR(255), old_status VARCHAR(255), new_status VARCHAR(255), transaction_date TIMESTAMP)");
+    }
 }
